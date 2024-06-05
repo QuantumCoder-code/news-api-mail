77b0df14668393107deb06a564184eec2925703d
QuantumCoder <gianlucaproto@gmail.com>
2024-06-05 15:48:59 +0200

    add new features

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
 from send_mail import send_mail
 
+topic = "tesla"
+
 url = "https://finance.yahoo.com"
 apy_key = "de131c1538df43d59eec035fd3362a4a"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-       "2024-05-01&sortBy=" \
-       "publishedAt&apiKey=de131c1538df43d59eec035fd3362a4a"
+url = "https://newsapi.org/v2/everything?" \
+       f"q={topic}&" \
+       "from=2024-05-05&" \
+       "sortBy=publishedAt&" \
+       "apiKey=de131c1538df43d59eec035fd3362a4a&language=en" \
+
 
 # Make request
 request = requests.get(url)
@@ -17,9 +22,11 @@ content = request.json()
 #Access the articles titles
 body = ""
 for article in content["articles"]:
-       body = body + (str(article["title"])
+       body = ("Subject: Today's news"
+                 + "\n" + body + (str(article["title"])
                  + "\n" + str(article["description"])
-                 + 2*"\n")
+                 + "\n " + str(article["url"])
+                 + 2*"\n"))
 
 body = body.encode("utf-8")
 send_mail(body)
